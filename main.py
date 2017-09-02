516ef5e4b05b6ef48b39bbab991abfbebae75402
Pavol Fulop <pavol.fulop@regex.sk>
2017-09-02 20:52:02 +0200

    Still some problem with nan

--- main.py
+++ main.py
@@ -4,22 +4,53 @@ from matplotlib import animation
 from datetime import datetime
 import tensorflow as tf
 import numpy as np
+import os
+from geopy.distance import vincenty
+from workalendar.usa import NewYork
+from workalendar.usa import UnitedStates
 
 predict = pd.read_csv('./inputs/test.csv')
-train = pd.read_csv('./inputs/train.csv').sample(n=1000)
+train = pd.read_csv('./inputs/train.csv').sample(n=10000)
+
+us_cal = UnitedStates()
+ny_cal = NewYork()
 
 
 # train = pd.read_csv('./inputs/train.csv')
 
-def strip_time(data):
-    t = data['pickup_datetime'].apply(
-        lambda d: pd.Series([datetime.strptime(d, "%Y-%m-%d %H:%M:%S").strftime("%H"),
-                             datetime.strptime(d, "%Y-%m-%d %H:%M:%S").strftime("%d%m")], index=['hour', 'date']))
-    return pd.concat([data, t], axis=1)
+def manipulate_row(row):
+    time = datetime.strptime(row['pickup_datetime'], "%Y-%m-%d %H:%M:%S")
+    row['hour'] = int(time.strftime("%H"))
+    row['date'] = int(time.strftime("%d%m"))
+    row['month'] = int(time.strftime("%m"))
+    row['weekday'] = int(time.strftime("%w"))
+    row['yearday'] = int(time.strftime("%U"))
+    row['year'] = int(time.strftime("%Y"))
+
+    row['distance'] = round(vincenty((row['pickup_latitude'], row['pickup_longitude']),
+                               (row['dropoff_latitude'], row['dropoff_longitude'])).kilometers,3)
+
+    row['holiday_us'] = int(us_cal.is_holiday(time) == True)
+    row['holiday_ny'] = int(ny_cal.is_holiday(time) == True)
+    row['blockade'] = int(time.strftime("%d%m") == "1109")
+    return row
 
 
-train = strip_time(train)
-predict = strip_time(predict)
+def prepare_data(data):
+    t = data.apply(manipulate_row, axis=1)
+    return t
+
+
+if not os.path.exists('./inputs/trained.csv') or not os.path.exists('./inputs/predicted.csv'):
+    train = prepare_data(train)
+    predict = prepare_data(predict)
+    train.to_csv('./inputs/trained.csv', index=False)
+    predict.to_csv('./inputs/predicted.csv', index=False)
+else:
+    predict = pd.read_csv('./inputs/predicted.csv')
+    train = pd.read_csv('./inputs/trained.csv')
+
+print(train.head())
 
 
 def get_time_period(i):
@@ -68,15 +99,17 @@ def show_hour_mean():
 def create_placeholders(n_x, n_y):
     Y = tf.placeholder(tf.float32, shape=(n_y, None), name="Y")
     X = tf.placeholder(tf.float32, shape=(n_x, None), name="X")
-    return X, Y
+    dropout_1 = tf.placeholder(tf.float32, name="dropout_1")
+    dropout_2 = tf.placeholder(tf.float32, name="dropout_2")
+    return X, Y, dropout_1, dropout_2
 
 
 def initialize_parameters():
-    W1 = tf.get_variable("W1", [25, 8], initializer=tf.contrib.layers.xavier_initializer())
+    W1 = tf.get_variable("W1", [25, 14], initializer=tf.contrib.layers.xavier_initializer())
     b1 = tf.get_variable("b1", [25, 1], initializer=tf.zeros_initializer())
-    W2 = tf.get_variable("W2", [12, 25], initializer=tf.contrib.layers.xavier_initializer())
-    b2 = tf.get_variable("b2", [12, 1], initializer=tf.zeros_initializer())
-    W3 = tf.get_variable("W3", [1, 12], initializer=tf.contrib.layers.xavier_initializer())
+    W2 = tf.get_variable("W2", [25, 25], initializer=tf.contrib.layers.xavier_initializer())
+    b2 = tf.get_variable("b2", [25, 1], initializer=tf.zeros_initializer())
+    W3 = tf.get_variable("W3", [1, 25], initializer=tf.contrib.layers.xavier_initializer())
     b3 = tf.get_variable("b3", [1, 1], initializer=tf.zeros_initializer())
 
     parameters = {"W1": W1,
@@ -86,12 +119,10 @@ def initialize_parameters():
                   "W3": W3,
                   "b3": b3}
 
-    print(parameters)
-
     return parameters
 
 
-def forward_propagation(X, parameters):
+def forward_propagation(X, dropout_1, dropout_2, parameters):
     W1 = parameters['W1']
     b1 = parameters['b1']
     W2 = parameters['W2']
@@ -99,18 +130,25 @@ def forward_propagation(X, parameters):
     W3 = parameters['W3']
     b3 = parameters['b3']
 
-    Z1 = tf.add(tf.matmul(W1, X), b1)
+    X_norm = tf.nn.l2_normalize(X, dim=0, name="X_normalized")
+    Z1 = tf.add(tf.matmul(W1, X_norm), b1)
     A1 = tf.nn.relu(Z1)
-    Z2 = tf.add(tf.matmul(W2, A1), b2)
+    D1 = tf.nn.dropout(
+        A1,
+        dropout_1,
+    )
+    Z2 = tf.add(tf.matmul(W2, D1), b2)
     A2 = tf.nn.relu(Z2)
-    Z3 = tf.add(tf.matmul(W3, A2), b3)
-    A3 = tf.nn.relu(Z3)
-    return A3
+    D2 = tf.nn.dropout(
+        A2,
+        dropout_2,
+    )
+    Z3 = tf.add(tf.matmul(W3, D2), b3)
+    return Z3
 
 
-def compute_cost(A3, Y):
-
-    cost = tf.sqrt(tf.reduce_mean(tf.square(tf.subtract(tf.log1p(A3), tf.log1p(Y)))))
+def compute_cost(Z3, Y):
+    cost = tf.sqrt(tf.reduce_mean(tf.square(tf.subtract(tf.log1p(Z3), tf.log1p(Y)))))
     return cost
 
 
@@ -124,17 +162,17 @@ def random_mini_batches(X, Y, size):
     return batches
 
 
-def model(X_train, Y_train, X_test, Y_test, learning_rate=0.0001,
-          num_epochs=1500, minibatch_size=128, print_cost=True, predict = []):
+def model(X_train, Y_train, X_test, Y_test, learning_rate=0.001,
+          num_epochs=1500, minibatch_size=32, print_cost=True, predict=[]):
     (n_x, m) = X_train.shape
     n_y = Y_train.shape[0]
     costs = []
 
-    X, Y = create_placeholders(n_x, n_y)
+    X, Y, dropout_1, dropout_2 = create_placeholders(n_x, n_y)
 
     parameters = initialize_parameters()
 
-    A3 = forward_propagation(X, parameters)
+    A3 = forward_propagation(X, dropout_1, dropout_2, parameters)
 
     cost = compute_cost(A3, Y)
 
@@ -158,7 +196,8 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate=0.0001,
 
             for minibatch in minibatches:
                 (minibatch_X, minibatch_Y) = minibatch
-                _, minibatch_cost = sess.run([optimizer, cost], feed_dict={X: minibatch_X, Y: minibatch_Y})
+                _, minibatch_cost = sess.run([optimizer, cost],
+                                             feed_dict={X: minibatch_X, Y: minibatch_Y, dropout_1: 0.65, dropout_2: 0.85})
 
                 epoch_cost += minibatch_cost / num_minibatches
 
@@ -172,7 +211,7 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate=0.0001,
         plt.ylabel('cost')
         plt.xlabel('iterations (per tens)')
         plt.title("Learning rate =" + str(learning_rate))
-        # plt.show()
+        plt.show()
 
         parameters = sess.run(parameters)
         print("Parameters have been trained!")
@@ -181,27 +220,24 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate=0.0001,
 
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
-        print("Train Accuracy:", accuracy.eval({X: X_train, Y: Y_train}))
-        print("Test Accuracy:", accuracy.eval({X: X_test, Y: Y_test}))
+        print("Train Accuracy:", accuracy.eval({X: X_train, Y: Y_train, dropout_1: 1, dropout_2: 1}))
+        print("Test Accuracy:", accuracy.eval({X: X_test, Y: Y_test, dropout_1: 1, dropout_2: 1}))
 
-
-        pred = sess.run(A3, feed_dict={X: predict})
+        pred = sess.run(A3, feed_dict={X: predict, dropout_1: 1, dropout_2: 1})
         saver.save(sess, './logs/nyc-model')
         t = pd.read_csv('./inputs/test.csv')
         output = pd.DataFrame()
         output['id'] = t['id']
-        print(pred[0])
-        print(pred.shape)
-        print(t['id'].values.shape)
         output['trip_duration'] = pred[0]
-        output[['id','trip_duration']].to_csv('./submit.csv', index = False)
+        output[['id', 'trip_duration']].to_csv('./submit.csv', index=False)
         sess.close()
         return parameters
 
 
 Y = train['trip_duration'].values
-train.drop(['trip_duration', 'store_and_fwd_flag', 'pickup_datetime', 'dropoff_datetime', 'id'], axis=1, inplace=True)
-predict.drop(['store_and_fwd_flag', 'pickup_datetime' , 'id'], axis=1, inplace=True)
+train.drop(['trip_duration', 'store_and_fwd_flag', 'pickup_datetime', 'dropoff_datetime', 'id', 'vendor_id',
+            'passenger_count'], axis=1, inplace=True)
+predict.drop(['store_and_fwd_flag', 'pickup_datetime', 'id', 'vendor_id', 'passenger_count'], axis=1, inplace=True)
 
 msk = np.random.rand(len(train)) < 0.8
 test = train[~msk]
@@ -209,6 +245,7 @@ train = train[msk]
 Y = np.reshape(Y, (1, Y.shape[0]))
 Ytest = Y[:, ~msk]
 Y = Y[:, msk]
+
 tf.reset_default_graph()
 model(train.values.T, Y, test.values.T, Ytest, predict=predict.values.T)
-
+os.system('say "your program has finished"')
